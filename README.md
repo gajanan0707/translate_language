32ee2c3201276df85c5bb980b16ee451b016543c
Jaydip Nakarani <jaydip@moveeasy.com>
2023-08-30 15:27:56 +0530

    Translate Language: - Setup Nodejs Project - Create a transalte api

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# translate_language
+
+# Language Translate (Cached Translation API with Redis)
+
+
+# Project Overview:
+The Cached Translation API with Redis is a service that provides efficient translation capabilities using the Microsoft Translator API while leveraging Redis for caching. This project aims to reduce API calls and improve response times by storing and retrieving translated text data from a Redis cache.
+
+
+## Installation
+
+Install translate_language with npm
+
+```bash
+  npm install translate_language
+  cd translate_language
+```
+    
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd translate_language
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`SUBSCRIPTION_KEY`
+
+`OCP_APIM_SUBSCRIPTION_REGION`
+
+
+## Badges
+
+Add badges from somewhere like: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
---
+++ routes/translate.js
@@ -0,0 +1,81 @@
+var axios = require("axios");
+const express = require("express");
+const router = express.Router();
+const redis = require("redis");
+
+// Redis config
+const redisClient = redis.createClient(6379, "127.0.0.1");
+redisClient.connect();
+redisClient.on("connect", function (err) {
+  console.log("Redis Connected");
+});
+
+router.post("/translate", async (req, res, next) => {
+  let queryParameter = req.query;
+  let translateTo = queryParameter.to;
+  let data = req.body;
+  let getCacheData = await redisClient.get(translateTo);
+  let foundTranslations = [];
+  let notFoundTranslations = [];
+
+  if (getCacheData) {
+    const cachedData = JSON.parse(getCacheData);
+    const mainTextsFromCache = cachedData.map((item) => item.main_text);
+    for (const element of data) {
+      const dataIndex = mainTextsFromCache.indexOf(element);
+      if (dataIndex !== -1) {
+        foundTranslations.push(cachedData[dataIndex]);
+      } else {
+        notFoundTranslations.push({
+          Text: element,
+        });
+      }
+    }
+  } else {
+    notFoundTranslations = data.map((text) => ({
+      Text: text,
+    }));
+  }
+  if (notFoundTranslations.length > 0) {
+    let config = {
+      method: "post",
+      url: `https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=${translateTo}`,
+      headers: {
+        "Ocp-Apim-Subscription-Key": process.env.SUBSCRIPTION_KEY,
+        "Content-Type": "application/json",
+        "Ocp-Apim-Subscription-Region":
+          process.env.OCP_APIM_SUBSCRIPTION_REGION,
+      },
+      data: notFoundTranslations,
+    };
+    await axios(config)
+      .then(async function (response) {
+        const convertedFormat = notFoundTranslations.map(
+          (main_text, index) => ({
+            from_lang: response.data[index].detectedLanguage.language,
+            to_lang: response.data[index].translations[0].to,
+            main_text: main_text.Text,
+            translate_text: response.data[index].translations[0].text,
+          })
+        );
+        const cachedData = JSON.parse(getCacheData);
+        if (cachedData) {
+          cachedData.push(...convertedFormat);
+          await redisClient.set(translateTo, JSON.stringify(cachedData));
+        } else {
+          await redisClient.set(translateTo, JSON.stringify(convertedFormat));
+        }
+        getCacheData = await redisClient.get(translateTo);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: JSON.parse(getCacheData),
+  });
+});
+
+module.exports = router;
