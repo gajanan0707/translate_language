68bd7116b1dcd96bd62d61d4cfbfd9155ccd12a3
Jaydip Nakarani <jaydip@moveeasy.com>
2023-08-31 12:23:55 +0530

    Changed readme file and added validation

--- README.md
+++ README.md
@@ -6,15 +6,6 @@
 The Cached Translation API with Redis is a service that provides efficient translation capabilities using the Microsoft Translator API while leveraging Redis for caching. This project aims to reduce API calls and improve response times by storing and retrieving translated text data from a Redis cache.
 
 
-## Installation
-
-Install translate_language with npm
-
-```bash
-  npm install translate_language
-  cd translate_language
-```
-    
 ## Run Locally
 
 Clone the project
@@ -35,12 +26,23 @@ Install dependencies
   npm install
 ```
 
+## Setup Redis
+[Documentation Redis Installation](https://redis.io/docs/getting-started/installation/install-redis-on-linux/)
+
+
 Start the server
 
 ```bash
   npm run start
 ```
+## Run the app
+```
+# Run in development mode
+npm run devStart
 
+# Run in production mode
+npm start
+```
 
 ## Environment Variables
 
@@ -51,10 +53,44 @@ To run this project, you will need to add the following environment variables to
 `OCP_APIM_SUBSCRIPTION_REGION`
 
 
+## API Reference
+To get the translation, we can Postman or any web browser and hit this API as follow:
+
+```http
+ # URL: (http://localhost:5000) or Deployed link
+
+ POST {URL}/translate?to=''
+```
+
+We have to pass one `Query Params` `targetLanguage`
+```
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `targetLanguage`      | `string` | **Required**.              |
+
+```
+We have to pass `body` with list of text's
+
+```
+[
+    "Hello How, are you",
+    "Hello, what is your name?"
+]
+```
+- `targetLanguage`: the langauge to be it has to be translated
+<br>**NOTE:**
+  - for the `targetLangauge`, only **ISO Language code** should be passed as value.
+<br/>For example:
+    - "hi" for Hindi
+    - "ja" for Japanese
+    - "fa" for Persian, etc.
+    <br/>A list of all [ISO Language Codes](https://datahub.io/core/language-codes/r/0.html) is attached here.
+
+## Screenshots
+
+![App Screenshot](https://i.imgur.com/46GxwKG.png)
+
 ## Badges
-
-Add badges from somewhere like: [shields.io](https://shields.io/)
-
 [![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
 [![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
 [![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
--- routes/translate.js
+++ routes/translate.js
@@ -7,75 +7,94 @@ const redis = require("redis");
 const redisClient = redis.createClient(6379, "127.0.0.1");
 redisClient.connect();
 redisClient.on("connect", function (err) {
-  console.log("Redis Connected");
+    console.log("Redis Connected");
+});
+
+router.get('/', async (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: "Translate api",
+    });
 });
 
 router.post("/translate", async (req, res, next) => {
-  let queryParameter = req.query;
-  let translateTo = queryParameter.to;
-  let data = req.body;
-  let getCacheData = await redisClient.get(translateTo);
-  let foundTranslations = [];
-  let notFoundTranslations = [];
-
-  if (getCacheData) {
-    const cachedData = JSON.parse(getCacheData);
-    const mainTextsFromCache = cachedData.map((item) => item.main_text);
-    for (const element of data) {
-      const dataIndex = mainTextsFromCache.indexOf(element);
-      if (dataIndex !== -1) {
-        foundTranslations.push(cachedData[dataIndex]);
-      } else {
-        notFoundTranslations.push({
-          Text: element,
+    let queryParameter = req.query;
+    let translateTo = queryParameter.targetLanguage;
+    if (!translateTo) {
+        return res.status(400).json({
+            success: false,
+            message: "targetLangauge is required in query parameter",
         });
-      }
     }
-  } else {
-    notFoundTranslations = data.map((text) => ({
-      Text: text,
-    }));
-  }
-  if (notFoundTranslations.length > 0) {
-    let config = {
-      method: "post",
-      url: `https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=${translateTo}`,
-      headers: {
-        "Ocp-Apim-Subscription-Key": process.env.SUBSCRIPTION_KEY,
-        "Content-Type": "application/json",
-        "Ocp-Apim-Subscription-Region":
-          process.env.OCP_APIM_SUBSCRIPTION_REGION,
-      },
-      data: notFoundTranslations,
-    };
-    await axios(config)
-      .then(async function (response) {
-        const convertedFormat = notFoundTranslations.map(
-          (main_text, index) => ({
-            from_lang: response.data[index].detectedLanguage.language,
-            to_lang: response.data[index].translations[0].to,
-            main_text: main_text.Text,
-            translate_text: response.data[index].translations[0].text,
-          })
-        );
-        const cachedData = JSON.parse(getCacheData);
-        if (cachedData) {
-          cachedData.push(...convertedFormat);
-          await redisClient.set(translateTo, JSON.stringify(cachedData));
-        } else {
-          await redisClient.set(translateTo, JSON.stringify(convertedFormat));
-        }
-        getCacheData = await redisClient.get(translateTo);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+    let data = req.body;
+    if (data.length > 0) {
+        let getCacheData = await redisClient.get(translateTo);
+        let foundTranslations = [];
+        let notFoundTranslations = [];
 
-  res.status(200).json({
-    success: true,
-    data: JSON.parse(getCacheData),
-  });
+        if (getCacheData) {
+            const cachedData = JSON.parse(getCacheData);
+            const mainTextsFromCache = cachedData.map((item) => item.main_text);
+            for (const element of data) {
+                const dataIndex = mainTextsFromCache.indexOf(element);
+                if (dataIndex !== -1) {
+                    foundTranslations.push(cachedData[dataIndex]);
+                } else {
+                    notFoundTranslations.push({
+                        Text: element,
+                    });
+                }
+            }
+        } else {
+            notFoundTranslations = data.map((text) => ({
+                Text: text,
+            }));
+        }
+        if (notFoundTranslations.length > 0) {
+            let config = {
+                method: "post",
+                url: `https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=${translateTo}`,
+                headers: {
+                    "Ocp-Apim-Subscription-Key": process.env.SUBSCRIPTION_KEY,
+                    "Content-Type": "application/json",
+                    "Ocp-Apim-Subscription-Region":
+                    process.env.OCP_APIM_SUBSCRIPTION_REGION,
+                },
+                data: notFoundTranslations,
+            };
+            await axios(config)
+                .then(async function (response) {
+                    const convertedFormat = notFoundTranslations.map(
+                        (main_text, index) => ({
+                            from_lang: response.data[index].detectedLanguage.language,
+                            to_lang: response.data[index].translations[0].to,
+                            main_text: main_text.Text,
+                            translate_text: response.data[index].translations[0].text,
+                        })
+                    );
+                    const cachedData = JSON.parse(getCacheData);
+                    if (cachedData) {
+                        cachedData.push(...convertedFormat);
+                        await redisClient.set(translateTo, JSON.stringify(cachedData));
+                    } else {
+                        await redisClient.set(translateTo, JSON.stringify(convertedFormat));
+                    }
+                    getCacheData = await redisClient.get(translateTo);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: JSON.parse(getCacheData),
+        });
+    }
+    res.status(400).json({
+        success: false,
+        messsage: "text is required in the body ",
+    });
 });
 
 module.exports = router;
